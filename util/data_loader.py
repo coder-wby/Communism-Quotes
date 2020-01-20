9fa1ecf1bf0e7c87ca004dc99fed22f1ef06199a
nevertiree <nevertiree@126.com>
2020-01-20 14:35:15 +0800

    设计了针对毛泽东选集的目录导入接口

--- util/data_loader.py
+++ util/data_loader.py
@@ -12,8 +12,14 @@ ROOT = os.path.abspath('../material')
 
 def load_origin_html_list(author) -> list:
 
+    valid_author_list = ["lenin-cworks", "maozedong"]
+
+    assert author in valid_author_list
+
+    # 作者文献的根目录
+    _root_path = os.path.join(ROOT, author)
+
     if author == 'lenin-cworks':  # 列宁全集
-        _root_path = os.path.join(ROOT, author)
         _vol_str_list = [_vol_str for _vol_str in os.listdir(_root_path)]
 
         _html_list = []
@@ -24,6 +30,13 @@ def load_origin_html_list(author) -> list:
                 continue
             _html_list.extend([os.path.join(_vol_root_path, _html_str) for _html_str in os.listdir(_vol_root_path)
                                if _html_str.endswith(".html")])
+
+        return _html_list
+
+    if author == 'maozedong':  # 毛泽东著作
+
+        _html_list = [os.path.join(_root_path, html_name) for html_name in os.listdir(_root_path)
+                      if html_name.endswith("html")]
 
         return _html_list
 
