f4288d3b67ea4e45e2890be775233f4ec32e0021
nevertiree <nevertiree@126.com>
2020-01-31 21:43:17 +0800

    增加了es的多词多域查询功能

--- elk/es_query.py
+++ elk/es_query.py
@@ -6,46 +6,7 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch()
 
-dsl_dict = {
-    "query": {
-        "bool": {
-            "must": [
-                # {"match": {"title": None}},
-                {"match": {"content": "走狗"}}
-            ],
-            "filter": [
-                {"term":  {"author": "列宁"}},
-                # {"range": {"publish_date": { "gte": "2015-01-01" }}}
-            ]
-        }
-    },
-    "highlight": {
-        "pre_tags": ["<tag1>", "<tag2>"],
-        "post_tags": ["</tag1>", "</tag2>"],
-        "fields": {
-            "content": {}
-        }
-    }
-}
-
-dsl_match = {
-    "query": {
-        "match": {
-            "content": {
-                "query": "美帝国主义",
-                "analyzer": "ik_smart",
-            },
-
-        }
-    },
-    "highlight": {
-        "pre_tags": ["<tag1>", "<tag2>"],
-        "post_tags": ["</tag1>", "</tag2>"],
-        "fields": {
-            "content": {}
-        }
-    }
-}
+# 参考文档 https://es.xiaoleilu.com/080_Structured_Search/10_compoundfilters.html
 
 
 def marxism_search(sql, index="marxism"):
@@ -54,6 +15,41 @@ def marxism_search(sql, index="marxism"):
                      size=20)
 
 
+def content_search(word_str: str, author_name="列宁", size=20):
+
+    word_str = word_str.strip()
+    word_str = word_str.replace("  ", " ")
+
+    sql = {
+        "query": {
+            "bool": {
+                "must": {
+                    "match": {
+                        "content": {
+                            "query": word_str,
+                            "minimum_should_match": "75%",
+                            "analyzer": "ik_smart",
+                        }
+                    }
+                },
+                "filter": [
+                    {"term": {"author": author_name}},
+                ]
+            }
+        },
+        "highlight": {
+            "pre_tags": ["<tag1>", "<tag2>"],
+            "post_tags": ["</tag1>", "</tag2>"],
+            "fields": {
+                "content": {}
+            }
+        }
+    }
+
+    return es.search(index="marxism", body=sql, size=size, _source_includes=["content", "title", "author"])
+
+
 if __name__ == '__main__':
-    result = marxism_search(dsl_match)
+    query_word = input("输入需要查询的词汇（多个词汇用空格隔开）")
+    result = content_search(query_word)
     print(json.dumps(result, indent=4, ensure_ascii=False))
